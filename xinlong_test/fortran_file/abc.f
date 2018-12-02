ef78fca5df3ca27748689e9a5de02ea950c91190
sailoridy <richardidy@gmail.com>
2018-12-02 10:28:55 -0500

    123

--- xinlong_test/fortran_file/abc.f
+++ xinlong_test/fortran_file/abc.f
@@ -1,7 +1,7 @@
       program abc
 
       implicit none
-      
+
       integer :: i, j, n
       integer, parameter :: nx = 640
       ! integer, parameter :: nr = (3*((nx/5)/2-0.5_dp_t)*((nx/5)/2-0.5_dp_t)-0.75_dp_t)/2.0_dp_t
@@ -9,3 +9,4 @@
       print *, nx
       print *, "I am Xinlong."
       end
+
