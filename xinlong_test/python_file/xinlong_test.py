5ea7f23f48863f53514c13f78d06fac4f34891ca
sailoridy <richardidy@gmail.com>
2018-12-02 10:24:06 -0500

    nothing

---
+++ xinlong_test/python_file/xinlong_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3])
+plt.show()
+
+a = 3
+b = np.power(a,a)
+
+print(b)
+print("Xinlong Li")
+
