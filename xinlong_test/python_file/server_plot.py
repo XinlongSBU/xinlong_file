ac4e786ed4e937dc3f6951b2353fe0bd7642f678
sailoridy <richardidy@gmail.com>
2018-12-02 11:44:03 -0500

    abc

--- xinlong_test/python_file/server_plot.py
+++ xinlong_test/python_file/server_plot.py
@@ -1,10 +1,14 @@
 import numpy as np
-import matplotlib
-matplotlib.use('tkagg')
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3])
-plt.show()
 
+import matplotlib as mpl
+mpl.use('Agg')
+
+import matplotlib.pyplot as plt
+
+
+plt.plot([1, 2, 3])
+#plt.show()
+plt.savefig("test.png") 
 a = 3
 b = np.power(a,a)
 
