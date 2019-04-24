4abe068f9cd224a8a728463f34f7ee083f51a5ce
sailor <richardidy@gmail.com>
2019-04-24 18:26:38 -0400

    burn cell python

--- python_library/tmp_build_dir/d/3d.gnu.EXE/AMReX_GpuControl.d
+++
@@ -1,2 +0,0 @@
-tmp_build_dir/o/3d.gnu.EXE/AMReX_GpuControl.o tmp_build_dir/d/3d.gnu.EXE/AMReX_GpuControl.d: /Users/sailor/Desktop/asro_work/git_fun/amrex/Src/Base/AMReX_GpuControl.cpp
-tmp_build_dir/o/3d.gnu.EXE/AMReX_GpuControl.o tmp_build_dir/d/3d.gnu.EXE/AMReX_GpuControl.d: /Users/sailor/Desktop/asro_work/git_fun/amrex/Src/Base/AMReX_GpuControl.H
